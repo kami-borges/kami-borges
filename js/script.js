340c91798b8fdb61374251153bb7b79cb4e471a8
kamilla passos borges <kamilla.borges.veleiros@gmail.com>
2025-04-03 16:03:40 -0300

    javscript

---
+++ js/script.js
@@ -0,0 +1,46 @@
+
+(function () { 
+
+"use strict";
+  /*O modo estrito ("use strict") é ativado para ajudar a detectar erros e comportamentos problemáticos no código. */
+
+  /**
+   * Função de seleção
+   */
+  
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function 
+   * Função de Adição de Eventos (on)
+   * Função espera que um evento ocorra e depois responde a ele  */
+  
+  
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  // Ativar Botao Menu no modo Mobile
+
+  on('click', '.mobile-nav-toggle', function() {
+    select('body').classList.toggle('mobile-nav-active') 
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+        })
+   })
+
+
